2787202cfe27a4d3c152f93bc94b5623f8d3a416
FadiRazouk <fadi.alrazouk@gmail.com>
2022-05-30 18:26:08 +0300

    addTests

---
+++ cypress/integration/ZenHR.spec.js
@@ -0,0 +1,23 @@
+import { loginPage } from "../locators/index";
+import pages from "../fixtures/pages.json";
+
+describe("", () => {
+  it(" @ID: 01", () => {
+    cy.visit("/en/users/sign_in");
+    cy.url().should("include", "/en");
+    cy.get(loginPage.userNameInput).type(pages.login.userName, { force: true })
+    cy.get(loginPage.passwordInput).type(pages.login.password, { force: true })
+    cy.get(loginPage.loginButton).click({ force: true })
+    cy.get(loginPage.snackbar).should("have.text", pages.login.successfulSignIn)
+    cy.get(loginPage.allLinks).each((link) => {
+      cy.request({
+        url: link.prop("href"),
+        failOnStatusCode: false,
+        headers: {
+          "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'"
+      }
+      })
+      .its("status").should("eq", 200)
+    })
+  });
+});
---
+++ cypress/locators/index.js
@@ -0,0 +1,9 @@
+export default {
+  loginPage: {
+    userNameInput:"#user_login",
+    passwordInput:"#user_password",
+    loginButton: ".login-submit > .btn",
+    allLinks:'li > a[href^="/en/"]',
+    snackbar: '#flash_notice'
+  },
+};
